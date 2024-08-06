3b6e503b98ce35f34c451a1992b07f20c3bdf2f7
Brian Turner <brianturnercs@gmail.com>
2024-08-06 15:21:45 -0600

    first commit

--- Ships.h
+++ Ships.h
@@ -1,12 +1,25 @@
 #include <iostream>
-
+#include <string>
 
 class Ships {
-    private:
+private:
+    std::string name;
     int health;
     int xPos;
     int yPos;
     bool vertical;
     
-
-}
+public:
+    Ships Ships();
+    Ships Ships(int type);
+    int getHealth();
+    void setHealth(int Heal);
+    int getX();
+    void setX(int newx);
+    int getY();
+    void setY(int newy);
+    bool getOrient();
+    void setOrient(bool newor);
+    std::string getName();
+    void setName(std::string neName);
+};
