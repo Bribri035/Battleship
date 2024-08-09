e52f713168444fdfcdb44b64368e0d1d7cbeaf11
Brian Turner <brianturnercs@gmail.com>
2024-08-09 15:14:38 -0600

    basic structure of ships completed

--- Ships.cpp
+++ Ships.cpp
@@ -1,1 +1,86 @@
-#include <Ships.h>
+#include "Ships.h"
+
+Ships::Ships(int type, int x, int y, bool direct)//create a ship at given area
+{
+    switch (type)
+    {
+    case 0://destroyer size2
+        health = 2;
+        name = "Destroyer";
+        /* code */
+        break;
+    case 1://cruiser size 3
+        health = 3;
+        name = "Cruiser";
+        break;
+    case 2://supmarine size 3
+        health = 3;
+        name = "Submarine";
+        break;
+    case 3://battleship size 4
+        health = 4;
+        name = "Battleship";
+        break;
+    case 4://carrier size 5
+        health = 5;
+        name = "Carrier";
+        break;
+    default://something went wrong, throw error
+        break;
+    }
+    //mark ships position
+    xPos = x;
+    yPos = y;
+    vertical = direct;
+}
+
+int Ships::getHealth(){
+    return health;
+}
+void Ships::setHealth(int Heal){
+    health = Heal;
+}
+int Ships::getX(){
+    return xPos;
+}
+void Ships::setX(int newx){
+    xPos = newx;
+}
+int Ships::getY(){
+    return yPos;
+}
+void Ships::setY(int newy){
+    yPos = newy;
+}
+bool Ships::getOrient(){
+    return vertical;
+}
+void Ships::setOrient(bool newor){
+    vertical = newor;
+}
+std::string Ships::getName(){
+    return name;
+}
+void Ships::setName(std::string neName){
+    name = neName;
+}
+int Ships::getDamage(){
+    return damage;
+}
+void Ships::setDamage(int neDam){
+    damage = neDam;
+}
+bool Ships::increaseDamage(){
+    damage++;
+    if (damage == health){
+        isalive = false;
+        return false;
+    }
+    return true;
+}
+bool Ships::getAlive(){
+    return isalive;
+}
+void Ships::setAlive(bool nealive){
+    isalive = nealive;
+}
--- Ships.h
+++ Ships.h
@@ -5,13 +5,14 @@ class Ships {
 private:
     std::string name;
     int health;
+    int damage = 0;
     int xPos;
     int yPos;
     bool vertical;
+    bool isalive = true;
     
 public:
-    Ships Ships();
-    Ships Ships(int type);
+    Ships(int type, int x, int y, bool direct);
     int getHealth();
     void setHealth(int Heal);
     int getX();
@@ -22,4 +23,10 @@ public:
     void setOrient(bool newor);
     std::string getName();
     void setName(std::string neName);
+    int getDamage();
+    void setDamage(int neDam);
+    bool increaseDamage(); //auto tell if ship is dead
+    bool getAlive();
+    void setAlive(bool nealive);
+
 };
